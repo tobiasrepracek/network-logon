819933d88348ebff09cb627de8baf866ee4f9e13
tobiasrepracek <80835891+tobiasrepracek@users.noreply.github.com>
2024-06-08 21:44:06 +0200

    Update logger.bat

--- client/logger.bat
+++ client/logger.bat
@@ -4,4 +4,4 @@ echo PC: %computername%
 for /f "tokens=2 delims=[]" %%f in ('ping -4 -n 1 %computername% ^| find /i "pinging"') do set IP=%%f
 echo IP: %IP%
 
-curl -s "http://192.168.200.46:89/?locip=%IP%&user=%username%&pc=%computername%"
+curl -s "http://server-ip:89/?locip=%IP%&user=%username%&pc=%computername%"
